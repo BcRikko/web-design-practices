753ea8603dd6f6a2f7d4b9be302e338a9c62ed34
bc_rikko <s.shin1106@gmail.com>
2017-03-20 18:24:17 +0900

    :tada: 環境設定

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const runSequence = require('run-sequence');
+const stylus = require('gulp-stylus');
+const plumber = require('gulp-plumber');
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'docs',
+            index: 'index.html'
+        }
+    });
+});
+
+gulp.task('sync', () => {
+    browserSync.reload();
+});
+
+gulp.task('stylus', () => {
+    return gulp.src('src/**/*.styl',{
+            base: 'src'
+        }).pipe(plumber())
+        .pipe(stylus())
+        .pipe(gulp.dest('./docs/'));
+});
+
+gulp.task('copy', () => {
+    return gulp.src(['src/**/*.*', '!src/**/*.styl'], {
+        base: 'src'
+    }).pipe(gulp.dest('./docs/'));
+});
+
+gulp.task('watch', ['browser-sync'], () => {
+    gulp.watch('src/**/*.*', () => {
+        runSequence(
+            ['copy', 'stylus'],
+            'sync'
+        );
+    });
+});
