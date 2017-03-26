d60b450bab411da808a3f93a40822819927653cf
bc_rikko <s.shin1106@gmail.com>
2017-03-26 09:50:43 +0900

    :+1: 出来事をスクロールに合わせて表示・非表示するようい修正

--- src/single-page-layout/js/script.js
+++ src/single-page-layout/js/script.js
@@ -1,7 +1,7 @@
-var pot = document.getElementsByClassName('flower-pot')[0];
-
 window.onscroll = function () {
+    var pot = document.getElementsByClassName('flower-pot')[0];
     var scrollTop = document.body.scrollTop;
+
     if (scrollTop < 460) {
         pot.className = 'flower-pot -begin-pos';
     } else if (460 <= scrollTop && scrollTop <= 1055) {
@@ -9,4 +9,19 @@ window.onscroll = function () {
     } else {
         pot.className = 'flower-pot -end-pos';
     }
+
+    var list = document.getElementsByClassName('event-item');
+    Array.prototype.forEach.call(list, function (a, i) {
+        var base = 500;
+        var date = a.getElementsByClassName('date')[0];
+        var desc = a.getElementsByClassName('description')[0]
+
+        if (i * 100 + 500 < scrollTop) {
+            date.classList.add('-show');
+            desc.classList.add('-show');
+        } else {
+            date.classList.remove('-show');
+            desc.classList.remove('-show');
+        }
+    });
 };
