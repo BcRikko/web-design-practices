0d1cb36dbbae1b11a04c7934bc19d705e0f53eea
bc_rikko <s.shin1106@gmail.com>
2017-03-27 19:25:56 +0900

    :tada: aboutセクションの追加

--- src/japanese-style/js/script.js
+++ src/japanese-style/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('#paging').onepage_scroll({
         sectionContainer: "section",
         direction: "horizontal",
+        pagination: true,
         animationTime: 500,
         loop: false
     });
