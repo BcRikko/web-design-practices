705e9583e2836cf9e92a16d21845b1b575dff189
BcRikko <s.shin1106@gmail.com>
2017-03-28 09:30:40 +0900

    :sparkles: Navigator機能を追加

--- src/japanese-style/js/script.js
+++ src/japanese-style/js/script.js
@@ -4,6 +4,18 @@ $(document).ready(function () {
         direction: "horizontal",
         pagination: true,
         animationTime: 500,
-        loop: false
+        loop: false,
+        afterMove: function (next) {
+            // beforeMoveとafterMoveのindexが同じ値になってしまうのでafterMoveで対応
+            $('.page-navigator > .menu').children().each(function (i, a) {
+                $(a).toggleClass('-active', i === next - 1);
+            });
+        }
+    });
+
+    $('.page-navigator > .menu').children().each(function (i, a) {
+        $(a).click(function () {
+            $('#paging').moveTo(i + 1);
+        });
     });
 });
